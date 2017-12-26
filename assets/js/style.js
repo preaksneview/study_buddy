60e9a8fe48d05b6b6079664658e33403cdca2f10
Jordan Boggs <boggsjp@gmail.com>
2017-12-25 21:41:33 -0700

    tweaked slide animation

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,16 +4,13 @@
 
 // Click CREATE NEW MEETING
 $("#add-new").on("click", function() {
-  // Hide the button
-  $("#add-new").slideToggle();
   // Show the form
-  $("#add-form").slideToggle();
+  $("#add-form").slideToggle(600);
 });
 
 // Click Submit in form
 $("#add-event").on("click", function() {
+  event.preventDefault();
   // Hide the form
-  $("#add-form").slideToggle();
-  // Show the button
-  $("#add-new").slideToggle();
+  $("#add-form").slideUp(600); 
 });
