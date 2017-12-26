647193b2e974d1b57cd2f90f2b320635b8bc4a80
Jordan Boggs <boggsjp@gmail.com>
2017-12-25 21:28:06 -0700

    Form now slides down

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,42 +3,8 @@
  */
 
 $("#add-new").on("click", function() {
-  $("main").slideDown(1000, function() {
-    $("main").html(`
-     <form>
-       <label for="name">Name</label><br/>
-       <input name="name" type="text"><br/>
-       <label for="date">Date &amp; Time</label><br/>
-       <input name="date" type="date"><br/>
-       <label for="duration">Duration (in minutes)</label><br/>
-       <input name="duration" type="number"><br/>
-       <label for="address">Address</label><br/>
-       <input name="address" type="text"><br/>
-       <input type="submit" id="add-event" name="Add event">
-     </form>
-   `).addClass('card')
-   .css({
-     'margin': '1vh',
-     'border': '1px solid #52c7b8',
-     'border-radius': '5px',
-     'padding': '1vh',
-     'line-height': '2rem'
-   });
-  });
-
-  $("input").css({
-    'border': '1px solid rgba(0,0,0,0.77)',
-    'border-radius': '5px'
-  });
-
-  $("#add-event").css({
-    'background-color': '#ff5252',
-    'border-radius': '5px',
-    'margin': '1vh 0',
-    'border': 0,
-    'padding': '5px 10px',
-    'height': '3rem',
-    'text-align': 'center',
-    'color': 'rgba(255,255,255,0.84)'
-  })
+  // Hide the button
+  $("#add-new").hide();
+  // Show the form
+  $("#add-form").slideDown();
 });
