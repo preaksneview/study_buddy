33afae33ea4683f71a94999b5392cf2f746b34ec
Jordan Boggs <boggsjp@gmail.com>
2017-12-25 21:32:38 -0700

    Animation uses slideToggle()

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,9 +2,18 @@
  * Styling and animation for StudyBuddy
  */
 
+// Click CREATE NEW MEETING
 $("#add-new").on("click", function() {
   // Hide the button
-  $("#add-new").hide();
+  $("#add-new").slideToggle();
   // Show the form
-  $("#add-form").slideDown();
+  $("#add-form").slideToggle();
+});
+
+// Click Submit in form
+$("#add-event").on("click", function() {
+  // Hide the form
+  $("#add-form").slideToggle();
+  // Show the button
+  $("#add-new").slideToggle();
 });
