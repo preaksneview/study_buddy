041da0bd6ee9e2919bf7c3ba95983b51e3695fe6
Dean Hooker <deanhooker91@gmail.com>
2018-01-04 20:36:22 -0700

    weather api

---
+++ assets/js/weather.js
@@ -0,0 +1,21 @@
+var API_key = '7669666f22479d98';
+
+
+    // Here we are building the URL we need to query the database
+    var queryURL = "http://api.wunderground.com/api/" + API_key + "/forecast/q/CO/Denver.json";
+
+    // We then created an AJAX call
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+
+      // Create CODE HERE to Log the queryURL
+      console.log(queryURL);
+      // Create CODE HERE to log the resulting object
+      console.log(
+        response
+      );
+
+
+    });
