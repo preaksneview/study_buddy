2983d70ec923d371cb9e2b40f2ef076c565a6c24
Jordan Boggs <boggsjp@gmail.com>
2018-01-12 13:37:09 -0700

    Fixed textarea rows issue and use of commas in description

--- assets/js/cloud.js
+++ assets/js/cloud.js
@@ -32,7 +32,7 @@ database.ref().on("child_added", function (childSnapshot, prevChildKey) {
   var prettyAddress = eventLocationStreet + " " + eventLocationCity + " " + eventLocationState + " " + eventLocationZip;
 
   //Put the data into the table on the app
-  $("#meeting-table").append("<tr data-description='" + eventDescription + "' data-fulladdress='"+prettyAddress+"'><td>" + eventName + "</td><td>" + eventDate + "</td><td>" +
+  $("#meeting-table").append('<tr data-description="' + eventDescription + '" data-fulladdress="'+prettyAddress+'"><td>' + eventName + '"</td><td>' + eventDate + '</td><td>' +
 
     eventDuration + "</td><td>" + eventLocationStreet + "</td><td>" + "<button class='expander' street=" + eventLocationStreetString + " city=" + eventLocationCityString + " state=" + eventLocationState + ">...</button></td>");
 
