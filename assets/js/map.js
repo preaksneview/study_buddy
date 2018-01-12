506af931a63e7a1de7ea581a25a5c127d1eb1727
Dean Hooker <deanhooker91@gmail.com>
2018-01-11 20:15:00 -0700

    added comments

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,41 +1,46 @@
+//initialise map
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map-section'), {
-    center: {lat: 39.7392, lng: -104.991},
-    zoom: 8
-  });
+    map = new google.maps.Map(document.getElementById('map-section'), {
+        center: { lat: 39.7392, lng: -104.991 },
+        zoom: 8
+    });
 }
-  
+
+//on expander click drop pin
 $(document).on("click", ".expander", function () {
 
+    //set pull variables from table
     let mapStreetAddress = $(this).attr("street");
     let mapCityAddress = $(this).attr("city");
     let mapStateAddress = $(this).attr("state");
-  
-    var queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + mapStreetAddress + ',' + mapCityAddress + ',' + mapStateAddress + '&key=AIzaSyBlgTkKRyGfGoEPtxLX-IfB31voIkAOOIY';
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function (response) {
-      console.log(response);
-  
-      let pinDrop = response.results[0].geometry.location;
-  
-      function initMap(pin) {
+    console.log(mapStreetAddress);
 
-        // var uluru = {lat: -25.363, lng: 131.044};
-        var pinDrop = pin
-        var map = new google.maps.Map(document.getElementById('map-section'), {
-          zoom: 12,
-          center: pinDrop
-        });
-        var marker = new google.maps.Marker({
-          position: pinDrop,
-          map: map
-        });
-      }  
+    var queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + mapStreetAddress + ',' + mapCityAddress + ',' + mapStateAddress + '&key=AIzaSyBlgTkKRyGfGoEPtxLX-IfB31voIkAOOIY';
+
+    //convert address to lat. and long. from Geocode API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+
+        let pinDrop = response.results[0].geometry.location;
+
+        //function to center map and drop pin
+        function initMap(pin) {
+
+            var pinDrop = pin
+            var map = new google.maps.Map(document.getElementById('map-section'), {
+                zoom: 12,
+                center: pinDrop
+            });
+            var marker = new google.maps.Marker({
+                position: pinDrop,
+                map: map
+            });
+        }
+        //run initMap with address pin dropping
         initMap(pinDrop);
     })
-  });
+});
 
